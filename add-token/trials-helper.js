425190e96009b88c15941fc599ba74a8f2c6e575
Jason Chase <chasej@chromium.org>
2017-03-20 10:32:01 -0400

    Replace token

--- add-token/trials-helper.js
+++ add-token/trials-helper.js
@@ -2,7 +2,7 @@
 
 var LongTaskToken = "AskgqD4TOQnfzHSqOQk1JuvALukPAm4X9GPpyCDoqzqrJUz5ijqvPSNgvkvOxt/otGgrss3eq0JHQnHWIXiWbgYAAABgeyJvcmlnaW4iOiAiaHR0cHM6Ly9qcGNoYXNlLmdpdGh1Yi5pbzo0NDMiLCAiZmVhdHVyZSI6ICJMb25nVGFza09ic2VydmVyIiwgImV4cGlyeSI6IDE0ODc4NjcxOTN9";
 // Expiry - seconds: 1493646650; date: 2017-05-01
-var USBToken = "AhLdUnb9/4gg3fScKzZxVloL1QQ8L0EanrlneFr5g5bni18+6tBgV0d0EXO8d7J9pmMWXfJjG8ICXho+SGPk8QsAAABSeyJvcmlnaW4iOiJodHRwczovL2pwY2hhc2UuZ2l0aHViLmlvOjQ0MyIsImZlYXR1cmUiOiJXZWJVU0IyIiwiZXhwaXJ5IjoxNDkzNjQ2NjUwfQ==";
+var USBToken = "AuLp/zGE6A5CmQzVCnmHxkH/du5RCmBmfCaX6CEXLj5pKI0mr6INBn1vv0/lFOEdE0oAoRCEomzVIeNbPSSK1wcAAABXeyJvcmlnaW4iOiAiaHR0cHM6Ly9qcGNoYXNlLmdpdGh1Yi5pbzo0NDMiLCAiZmVhdHVyZSI6ICJXZWJVU0IyIiwgImV4cGlyeSI6IDE0OTM2NDY2NTB9";
 
 function addTrialToken(token) {
   var tokenElement = document.createElement('meta');
