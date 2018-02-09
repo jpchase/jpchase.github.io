d4864e24f17daceb5bcaf6be558c64b7b6cf3725
Jason Chase <chasej@chromium.org>
2018-02-08 20:14:56 -0500

    Updates to semi-current trials

--- add-token/trials-helper.js
+++ add-token/trials-helper.js
@@ -2,7 +2,7 @@
 
 var LongTaskToken = "AskgqD4TOQnfzHSqOQk1JuvALukPAm4X9GPpyCDoqzqrJUz5ijqvPSNgvkvOxt/otGgrss3eq0JHQnHWIXiWbgYAAABgeyJvcmlnaW4iOiAiaHR0cHM6Ly9qcGNoYXNlLmdpdGh1Yi5pbzo0NDMiLCAiZmVhdHVyZSI6ICJMb25nVGFza09ic2VydmVyIiwgImV4cGlyeSI6IDE0ODc4NjcxOTN9";
 // Expiry - seconds: 1493646650; date: 2017-05-01
-var USBToken = "AuLp/zGE6A5CmQzVCnmHxkH/du5RCmBmfCaX6CEXLj5pKI0mr6INBn1vv0/lFOEdE0oAoRCEomzVIeNbPSSK1wcAAABXeyJvcmlnaW4iOiAiaHR0cHM6Ly9qcGNoYXNlLmdpdGh1Yi5pbzo0NDMiLCAiZmVhdHVyZSI6ICJXZWJVU0IyIiwgImV4cGlyeSI6IDE0OTM2NDY2NTB9";
+var WebBudgetToken = "AuLp/zGE6A5CmQzVCnmHxkH/du5RCmBmfCaX6CEXLj5pKI0mr6INBn1vv0/lFOEdE0oAoRCEomzVIeNbPSSK1wcAAABXeyJvcmlnaW4iOiAiaHR0cHM6Ly9qcGNoYXNlLmdpdGh1Yi5pbzo0NDMiLCAiZmVhdHVyZSI6ICJXZWJVU0IyIiwgImV4cGlyeSI6IDE0OTM2NDY2NTB9";
 
 function addTrialToken(token) {
   var tokenElement = document.createElement('meta');
@@ -11,28 +11,27 @@ function addTrialToken(token) {
   document.head.appendChild(tokenElement);
 }
 
-function detectWebUSB() {
-  if (navigator.usb) {
-    console.log('WebUSB already enabled.');
+function detectWebBudget() {
+  if (navigator.budget.getBudget) {
+    console.log('WebBudget already enabled.');
     return true;
   }
-  console.log('WebUSB is not yet enabled.');
+  console.log('WebBudget is not yet enabled.');
   return false;
 }
 
-function tryWebUSB() {
+function tryWebBudget() {
   window.setTimeout(function() {
-    if (!navigator.usb) {
-      throw "No WebUSB support.";
+    if (!navigator.budget.getBudget) {
+      throw "No WebBudget support.";
     }
-    navigator.usb.getDevices().then(devices => {
-      console.log('Found devices:', devices);
-      devices.map(device => {
-        console.log(device.productName);      // "Arduino Micro"
-        console.log(device.manufacturerName); // "Arduino LLC"
+    navigator.budget.getBudget()
+    .then((budgets) => {
+      budgets.forEach((element) => {
+        console.log(`At '${new Date(element.time).toString()}' your budget will be '${element.budgetAt}'.`);
       });
     })
-    .catch(error => console.log('Error getting devices:', error));
+    .catch(error => console.log('Error getting budget:', error));
   }, 1);
 }
 
